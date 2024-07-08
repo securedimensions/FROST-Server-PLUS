9bd376fd7b2c5fb83f606c325b88d9c00e5defcc
Andreas Matheus <andreas.matheus@secure-dimensions.de>
2024-07-08 15:51:51 +0200

    license header update

--- src/test/java/de/securedimensions/frostserver/plugin/staplus/test/CampaignTests.java
+++ src/test/java/de/securedimensions/frostserver/plugin/staplus/test/CampaignTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021-2023 Secure Dimensions GmbH, D-81377
+ * Copyright (C) 2021-2024 Secure Dimensions GmbH, D-81377
  * Munich, Germany.
  *
  * This program is free software: you can redistribute it and/or modify
